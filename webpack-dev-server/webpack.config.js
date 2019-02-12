6dcf5bf940b9b41f8af38e0f3684a1145c9a2a55
Gian Pumayalla <g.pumayalla@hotmail.com>
2019-02-12 15:43:28 +0100

    webpack dev server

---
+++ webpack-dev-server/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: path.resolve(__dirname, 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    port: 9000,
+  },
+  module: {
+    rules:[
+      //Qui andranno i loader
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader'],
+      }
+    ]
+  }
+}
